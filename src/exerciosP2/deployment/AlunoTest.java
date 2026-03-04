c7a5a40d3fd08129ae222c3ff5903c9f76752456
caiodevzz <caiomateus270@gmail.com>
2026-03-03 21:19:03 -0300

    novas funcionalidades

--- src/exerciosP2/deployment/AlunoTest.java
+++ src/exerciosP2/deployment/AlunoTest.java
@@ -15,8 +15,8 @@ public class AlunoTest {
 
         List<Aluno> alunos = new ArrayList<>();
 
-        for (int i = 0; i < n ; i++) {
-            System.out.println("Aluno: " + (i +1));
+        for (int i = 0; i < n; i++) {
+            System.out.println("Aluno: " + (i + 1));
 
             System.out.print("Id: ");
             int id = sc.nextInt();
@@ -29,7 +29,7 @@ public class AlunoTest {
             System.out.print("Nota: ");
             Double nota = sc.nextDouble();
 
-            alunos.add(new Aluno(id , nome , nota));
+            alunos.add(new Aluno(id, nome, nota));
         }
 
         System.out.print("Qual id voce deseja fazer altercacao de nota ?");
@@ -51,12 +51,34 @@ public class AlunoTest {
 
         if (aluno == null) {
             System.out.println("Aluno nao encontrado");
-        }else {
+        } else {
             aluno.alteracaoDeNotas(nota);
         }
 
         System.out.println("Lista atualizada com as notas: ");
 
+        for (Aluno x : alunos) {
+            System.out.println(x);
+        }
+
+        System.out.print("Voce deseja remover algum aluno? (Sim/Nao): ");
+        String remocao = sc.nextLine();
+
+        if (remocao.equalsIgnoreCase("Sim")) {
+            System.out.print("De qual posicao? ");
+            int posicao = sc.nextInt();
+            sc.nextLine();
+
+            try {
+                Aluno removido = alunos.remove(posicao);
+                System.out.println("Aluno " + removido + " removido com sucesso!");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Erro: Posicao invalida!");
+            }
+        }
+
+
+        System.out.println("Lista atualizada:");
         for (Aluno x : alunos) {
             System.out.println(x);
         }
